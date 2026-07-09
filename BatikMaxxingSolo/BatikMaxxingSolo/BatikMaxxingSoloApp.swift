599a668ef1965db3e55a013fb87ff20411ec73f0
LiecardoAntonio <liecardoantonio24@gmail.com>
2026-07-09 22:11:58 +0700

    Implementing SwiftData for Canvas saving

---
+++ BatikMaxxingSolo/BatikMaxxingSolo/BatikMaxxingSoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatikMaxxingSoloApp.swift
+//  BatikMaxxingSolo
+//
+//  Created by Liecardo on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BatikMaxxingSoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
