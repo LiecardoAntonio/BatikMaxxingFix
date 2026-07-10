273b5faabdd59874aa408b4744e1ad0b674c88f0
Asaryun13 <164145295+Asaryun13@users.noreply.github.com>
2026-07-10 15:38:43 +0700

    Library

---
+++ BatikMaxxingSolo/BatikMaxxingSolo/Models/LibraryModel.swift
@@ -0,0 +1,8 @@
+//
+//  LibraryModel.swift
+//  BatikMaxxingSolo
+//
+//  Created by James Richard Renaldo on 10/07/26.
+//
+
+import Foundation
---
+++ BatikMaxxingSolo/BatikMaxxingSolo/Views/Library/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  BatikMaxxingSolo
+//
+//  Created by James Richard Renaldo on 10/07/26.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryView()
+}
